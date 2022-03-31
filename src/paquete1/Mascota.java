2c2c80233f198be44dd9e8521e7c4bbc00c2d9e0
Amanda <amandaroblesurena@gmail.com>
2022-03-31 21:02:22 +0200

    Original

---
+++ src/paquete1/Mascota.java
@@ -0,0 +1,23 @@
+package paquete1;
+
+public class Mascota {
+	private String nombre;
+
+	public Mascota(String nombre) {
+		this.nombre = nombre;
+
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void MandarMensaje() {
+		System.out.println("Soy " + this.nombre + " y te voy a contar algo o no");
+	}
+
+}
