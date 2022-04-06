d891d28d8c9222ecc67f9fc2b8fce7b762b8600e
Amanda <amandaroblesurena@gmail.com>
2022-04-06 19:57:12 +0200

    Añadida clase Mono- AMANDA

---
+++ src/paquete1/Mono.java
@@ -0,0 +1,15 @@
+package paquete1;
+
+public class Mono extends Mascota{
+	
+	
+	public Mono(String nombre) {
+		super(nombre);
+		
+	}
+
+	
+	
+
+
+}
--- src/paquete1/Principal.java
+++ src/paquete1/Principal.java
@@ -2,9 +2,10 @@ package paquete1;
 public class Principal {
 	public static void main(String[] args) {
 		
-
+		Mono mono1=new Mono("Amelio");
+		
+		mono1.MandarMensaje();
 	
-
 	}
 
 }
